7c6ae7d5872ab58188da942d7ebb22c0239b6354
Maria Morales <maremarismaria@gmail.com>
2022-02-20 07:49:52 +0000

    :adhesive_bandage: Update component name

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import MoodTracker from "@/components/MoodTracker.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <MoodTracker />
   </main>
 </template>
