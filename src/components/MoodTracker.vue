bf329fa8dac1c31d0b815db95b946f0c06a23a18
Maria Morales <maremarismaria@gmail.com>
2022-02-22 08:17:19 +0000

    :sparkles: Implement state management and persistent storage into the mood tracker

--- src/components/MoodTracker.vue
+++ src/components/MoodTracker.vue
@@ -1,10 +1,57 @@
 <script setup lang="ts">
-function getRandomColor() {
-  const decimalWhite = 16777215;
-  const code = Math.floor(Math.random() * decimalWhite).toString(16);
-  const color = Math.random() > 0.5 ? "rgba(60, 60, 60, 0.2)" : `#${code}`;
-  return `background-color: ${color}`;
-}
+import { ref, onMounted, onUpdated } from "vue";
+import type { Ref } from "@vue/reactivity";
+import type { DayInfo } from "@/types";
+import { getStore, updateStore } from "@/utils/Storage";
+import { getRandomHexColor } from "@/utils/Color";
+
+const data: Ref<DayInfo[]> = ref([]);
+
+const setInitialData = () => {
+  const _data = getStore();
+  data.value = _data ? _data : data.value;
+};
+
+const updateData = ({ day, month, color }: DayInfo) => {
+  const alreadyStored = data.value.some(
+    (element) => day === element.day && month === element.month
+  );
+
+  if (!alreadyStored) {
+    data.value = [...data.value, { day, month, color }];
+  }
+};
+
+onMounted(() => {
+  setInitialData();
+});
+
+onUpdated(() => {
+  // console.log(data.value);
+});
+
+const onClickDay: (info: DayInfo, e: MouseEvent) => void = (info) => {
+  const color = getRandomHexColor();
+  const _data = { ...info, color };
+  updateStore(_data);
+  updateData(_data);
+};
+
+const setDayColor = ({ day, month }: DayInfo) => {
+  let matchedElement = {} as DayInfo;
+
+  for (let i = 0; i < data.value.length; i++) {
+    const element = data.value[i];
+    if (day === element.day && month === element.month) {
+      matchedElement = { ...element };
+      break;
+    }
+  }
+
+  if (matchedElement) {
+    return `background-color: ${matchedElement.color}`;
+  }
+};
 </script>
 
 <template>
@@ -15,7 +62,12 @@ function getRandomColor() {
     </tr>
     <tr v-for="day in 31" :key="day">
       <th>{{ day }}</th>
-      <td v-for="month in 12" :key="month" v-bind:style="getRandomColor()"></td>
+      <td
+        v-for="month in 12"
+        :key="month"
+        @click="onClickDay({ day, month }, $event)"
+        v-bind:style="setDayColor({ day, month })"
+      ></td>
     </tr>
   </table>
 </template>
@@ -33,14 +85,20 @@ table {
 tr {
   grid-template-columns: repeat(13, minmax(32px, 1fr));
 }
-th {
-  font-weight: bold;
-}
 
 th,
 td {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+th {
+  font-weight: bold;
+}
+td {
+  cursor: pointer;
+  border: 1px solid var(--vt-c-black);
+  background-color: var(--vt-c-black-transparent);
 }
 </style>
