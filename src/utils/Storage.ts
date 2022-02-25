1b27a38fb20c68c544e947048dd70d9d94cb5468
Maria Morales <maremarismaria@gmail.com>
2022-02-25 07:45:50 +0000

    :fire: Remove specific logic from a common place

--- src/utils/Storage.ts
+++ src/utils/Storage.ts
@@ -6,18 +6,8 @@ export const getStore: () => DayInfo[] = () => {
   return store ? JSON.parse(store) : undefined;
 };
 
-export const updateStore = (content: DayInfo) => {
-  const store = getStore();
-  let _content: DayInfo[] = [content];
-
-  if (store) {
-    const alreadyStored = store.some(
-      ({ day, month }) => day === content.day && month === content.month
-    );
-    _content = alreadyStored ? [...store] : [...store, ..._content];
-  }
-
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(_content));
+export const updateStore = (content: DayInfo[]) => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(content));
 };
 
 export const deleteStore = () => {
